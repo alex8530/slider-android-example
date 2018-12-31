16b44037c679c27fa29181d93d097d0a461ea9f5
Alex <Aliabuharb8530@gmil.com>
2018-12-31 22:24:51 +0200

    init

---
+++ app/src/main/java/com/example/justdoit/imageslideresample/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.justdoit.imageslideresample;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bumptech.glide.request.RequestOptions;
+
+import com.glide.slider.library.SliderLayout;
+import com.glide.slider.library.SliderTypes.BaseSliderView;
+import com.glide.slider.library.SliderTypes.TextSliderView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements BaseSliderView.OnSliderClickListener {
+    SliderLayout mDemoSlider;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDemoSlider = findViewById(R.id.slider);
+        ArrayList<String> listUrl = new ArrayList<>();
+        ArrayList<String> listName = new ArrayList<>();
+
+        listUrl.add("https://www.revive-adserver.com/media/GitHub.jpg");
+        listName.add("JPG - Github");
+
+        listUrl.add("https://tctechcrunch2011.files.wordpress.com/2017/02/android-studio-logo.png");
+        listName.add("PNG - Android Studio");
+
+        listUrl.add("http://static.tumblr.com/7650edd3fb8f7f2287d79a67b5fec211/3mg2skq/3bdn278j2/tumblr_static_idk_what.gif");
+        listName.add("GIF - Disney");
+
+        listUrl.add("http://www.gstatic.com/webp/gallery/1.webp");
+        listName.add("WEBP - Mountain");
+
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions.centerCrop();
+        for (int i = 0; i < listUrl.size(); i++) {
+            TextSliderView sliderView = new TextSliderView(this);
+            // if you want show image only / without description text use DefaultSliderView instead
+
+            // initialize SliderLayout
+            sliderView
+                    .image(listUrl.get(i))
+                    .description(listName.get(i))
+                    .setRequestOption(requestOptions)
+                    .setBackgroundColor(Color.WHITE)
+                    .setProgressBarVisible(true)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            sliderView.bundle(new Bundle());
+            sliderView.getBundle().putString("extra", listName.get(i));
+            mDemoSlider.addSlider(sliderView);
+        }
+
+    }
+
+
+    @Override
+    public void onSliderClick(BaseSliderView baseSliderView) {
+
+    }
+}
---
+++ app/src/main/java/com/example/justdoit/imageslideresample/MyApplication.java
@@ -0,0 +1,12 @@
+package com.example.justdoit.imageslideresample;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+     }
+}
